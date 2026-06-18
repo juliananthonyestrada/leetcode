6de503a4d4f6adc57e9ffd3378bf227cede15116
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-06-18 14:50:19 -0400

    Add: monotonic-array - submission-8

---
+++ Data Structures & Algorithms/monotonic-array/submission-8.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+
+        mon_inc, mon_dec = True, True
+
+        i, j = 0, 1
+
+        # iterate over once
+        # if at any point the condition is violated
+        # swap the boolean
+        while j < len(nums):
+            if nums[i] > nums[j]:
+                mon_inc = False
+            elif nums[i] < nums[j]:
+                mon_dec = False    
+            if not (mon_dec or mon_inc):
+                return False
+            i += 1
+            j += 1
+
+        return mon_inc or mon_dec
