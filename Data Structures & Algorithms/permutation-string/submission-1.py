64612646f867db6be203bd21f1c9a426e0d60392
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-06-22 16:29:26 -0400

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        # first thought is a freq map and a sliding window
+        # added insight: have the window be of a fixed size
+            # there is no need to allow it to expand or shrink to a size it cannot be
+
+        freq = {}
+
+        for char in s1:
+            if char not in freq:
+                freq[char] = 1
+            else:
+                freq[char] += 1
+
+        def check_window(sub):
+            for char in sub:
+                if char not in s1:
+                    return False
+                elif sub.count(char) != freq[char]:
+                    return False
+            return True
+
+        left, right = 0, len(s1)-1
+
+        while right < len(s2):
+            if check_window(s2[left:right+1]):
+                return True
+            left += 1
+            right += 1
+        
+        return False
