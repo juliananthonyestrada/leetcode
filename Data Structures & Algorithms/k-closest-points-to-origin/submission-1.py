4eeb81afa4827bc65d8bbf00a17b1f4190702a18
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-06-30 15:54:46 -0400

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    import math
+    import heapq
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        
+        # create a min heap
+        # what we compare nodes on is euclidean distance to the origin
+        # the root of the min heap is the point closes to the origin 
+        # we will store in the min heap the distance
+        # and then have a hash map -> distance : point
+        res = []
+        heap = []
+        heapq.heapify(heap)
+
+        for point in points:
+            distance = math.sqrt((point[0])**2 + (point[1])**2)
+            heapq.heappush(heap, (-distance, point))
+            
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        while heap:
+            res.append(heapq.heappop(heap)[1])
+        
+        return res            
+
