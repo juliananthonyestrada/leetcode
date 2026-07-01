9a51904bf49f22dee690d3124cd408d651dcd244
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-07-01 14:23:26 -0400

    Add: maximum-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-subarray/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # what is the max subarray ending at idx i ?
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+
+        for i in range(1, len(nums)):
+            dp[i] = max(dp[i-1] + nums[i], nums[i])
+    
+        return max(dp)
