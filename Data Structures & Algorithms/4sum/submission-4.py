12699c6133dd2ae023895a784834f9e3722f7433
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-07-15 08:19:21 -0400

    Add: 4sum - submission-4

---
+++ Data Structures & Algorithms/4sum/submission-4.py
@@ -0,0 +1,43 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+        # fix a pair and then among the remaining elements search for a pair
+        
+        def get_valid_ptrs(i, j, left, right):
+            if left == i:
+                left += 1
+            if left == j:
+                left += 1
+            if right == j:
+                right -= 1
+            if right == i:
+                right -= 1
+            return left, right
+                
+        
+        l = len(nums)
+        nums.sort()
+        quads = set()
+
+        # fix a pir
+        for i in range(l):
+            for j in range(i+1, l):
+                left, right = 0, l-1
+
+                # linear scan to find complementary pair 
+                while left < right:
+                    left, right = get_valid_ptrs(i, j, left, right)
+                    if left == right:
+                        break
+
+                    curr_quad = nums[i] + nums[j] + nums[left] + nums[right]
+                    if curr_quad == target:
+                        quads.add(tuple(sorted([nums[i], nums[j], nums[left], nums[right]])))
+                        left += 1
+                        right -= 1
+                    elif curr_quad > target:
+                        right -= 1
+                    else:
+                        left += 1
+
+        return list(quads)        
