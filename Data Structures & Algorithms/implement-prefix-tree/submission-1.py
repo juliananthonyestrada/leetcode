6f3ee46dd2592f15fb99b2899b9bbc18afa2904d
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-07-27 16:51:32 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, word: str) -> None:
+        root = self.root
+        for i in range(len(word)):
+            if word[i] not in root.children:
+                root.children[word[i]] = Node()
+            root = root.children[word[i]]
+            
+        root.isWord = True
+            
+    def search(self, word: str) -> bool:
+        root = self.root
+        for i in range(len(word)):
+            if word[i] in root.children:
+                root = root.children[word[i]]
+                if i == len(word) -1:
+                    return root.isWord
+            else:
+                return False
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        root = self.root
+        for i in range(len(prefix)):
+            if prefix[i] in root.children:
+                root = root.children[prefix[i]]
+                if i == len(prefix) - 1:
+                    return True
+            else:
+                return False
+        
+        
