1b09433694786fbc83bd56e0133f146f53464d90
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-08-22 13:08:36 -0400

    Add: sum-of-all-subset-xor-totals - submission-1

---
+++ Data Structures & Algorithms/sum-of-all-subset-xor-totals/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def xor_total(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        running = nums[0]
+
+        for num in nums[1:]:
+            running ^= num
+        
+        return running
+
+    def subsetXORSum(self, nums: List[int]) -> int:
+        
+        res = 0
+
+        def subsets(idx, sub):
+            nonlocal res
+
+            # last element in array - does not track size of sub - we can reach the last element w all no decisions
+            if idx == len(nums):
+                res += self.xor_total(sub)
+                return
+            
+            # take
+            sub.append(nums[idx])
+            subsets(idx+1, sub)
+            
+            # dont take
+            sub.pop()
+            subsets(idx+1, sub)
+
+        subsets(0, [])
+        return res
