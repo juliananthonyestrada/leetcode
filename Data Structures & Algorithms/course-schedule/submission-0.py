8d70061e109da335b8bd7fea42434e24e2a7ceb4
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-08-29 19:32:01 -0400

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.py
@@ -0,0 +1,46 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        # return true if we can produce a valid topological ordering of size num courses
+
+        def topological_sort():
+            path = set()
+            visited = set()
+            adj = {i:[] for i in range(numCourses)}
+
+            for src, dst in prerequisites:
+                adj[src].append(dst)
+            
+            for src in adj:
+                if not dfs(src, visited, adj, path):
+                    return False
+            return True
+            
+        def dfs(src, visited, adj, path):
+            if src in path:
+                return False
+
+            if src in visited:
+                return True
+
+            path.add(src)
+            visited.add(src)
+
+            for dst in adj[src]:
+                if not dfs(dst, visited, adj, path):
+                    return False
+
+            path.remove(src)
+
+            return True
+
+        return topological_sort()
+
+
+
+
+
+
+
+
+
