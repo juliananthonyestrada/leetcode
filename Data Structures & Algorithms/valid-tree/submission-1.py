47e61d273114ddd6f7b15f3e6be50cdecb4e9834
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-08-30 16:23:59 -0400

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not edges: return True
+        if len(edges) > n-1: return False
+        
+        adj = {i:[] for i in range(n)}
+        for src, dst in edges:
+            adj[src].append(dst)
+            adj[dst].append(src)
+
+        visited = set()
+        def acyclic(node, parent):
+            if node in visited:
+                return False
+            
+            visited.add(node)
+            for nghb in adj[node]:
+                if nghb == parent:
+                    continue
+                elif not acyclic(nghb, node):
+                    return False
+
+            return True
+    
+        return acyclic(0, None) and len(visited) == n
+        
+        
+        
+            
+
+
+
+
+
+
+
+
+
+
+        # do i need path as an arg ??
