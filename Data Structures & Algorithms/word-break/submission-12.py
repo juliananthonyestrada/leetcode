9cfd0760e3d30cf32f33bf4274c8faab8023666a
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-09-04 12:50:32 -0400

    Add: word-break - submission-12

---
+++ Data Structures & Algorithms/word-break/submission-12.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        slen = len(s)
+
+        cache = {}
+
+        def can_segment(i):
+            if i >= slen:
+                return True
+
+            if i in cache:
+                return cache[i]
+            
+            for word in wordDict:
+                if s[i : i + len(word)] == word:
+                    if can_segment(i + len(word)):
+                        cache[i] = True
+                        return True
+
+            cache[i] = False
+            return cache[i]
+        
+        return can_segment(0)
