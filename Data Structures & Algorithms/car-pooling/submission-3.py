8e23207a16a1fa86c7be74ba14ba3687f69820b8
Julian Anthony Estrada <160971152+juliananthonyestrada@users.noreply.github.com>
2026-09-08 11:09:31 -0400

    Add: car-pooling - submission-3

---
+++ Data Structures & Algorithms/car-pooling/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        size = 0
+        heap = []   # (dropoff time, num passengers)
+        trips.sort(key = lambda t : t[1])
+
+        for passengers, pickup, dropoff in trips:
+            # drop off passengers 
+            while heap and heap[0][0] <= pickup:
+                size -= heapq.heappop(heap)[1] 
+
+            heapq.heappush(heap, (dropoff, passengers)) 
+
+            # pick up curr passengers 
+            size += passengers
+            if size > capacity:
+                return False
+        
+        return True
